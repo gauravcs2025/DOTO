f20d4f00eb65a24ad5ad81ec1ba4ad6ccd3d0069
gauravcs2025 <166909849+gauravcs2025@users.noreply.github.com>
2025-12-01 16:11:29 +0000

    added email tracking to every datalayer push

--- assets/script.js
+++ assets/script.js
@@ -1,231 +1,300 @@
-var cartCount= 1;
+var userEmail = localStorage.getItem("user_email") || "guest";
+var cartCount = 1;
 var url = window.location.href;
 var title = document.title;
 var hostName = window.location.host;
-var pageName = url.substring(url.lastIndexOf("/")+1);
-var months = ['January','February','March','April','May','June','July','August','September','October','November','December']
-var Day = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+var pageName = url.substring(url.lastIndexOf("/") + 1);
+var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+var Day = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 var currDate = new Date();
 var formStart = 1;
-document.getElementById("readMore").addEventListener('click',(e)=>{
-    document.getElementById("description").textContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+document.getElementById("readMore").addEventListener("click", (e) => {
+  document.getElementById("description").textContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 });
 
-document.getElementById("submit").addEventListener('click',(e)=>{
-    e.preventDefault();
+document.getElementById("submit").addEventListener("click", (e) => {
+  e.preventDefault();
 });
 
+window.addEventListener("load", () => {
+  adobeDataLayer.push({
+    event: "aa-pageload",
+    user: {
+      email: userEmail,
+    },
+    page: {
+      pageInfo: {
+        pageName: title,
+        pageUrl: url,
+        referrer: document.referrer,
+        hostName: hostName,
+        language: "en-us",
+        errorPage: false,
+        pageType: title,
+        pageId: title + " " + 123,
+      },
+    },
+    timeStamp: {
+      date: currDate.toDateString(),
+      day: Day[currDate.getDay()],
+      month: months[currDate.getMonth()],
+      time:
+        currDate.getHours() +
+        ":" +
+        currDate.getMinutes() +
+        ":0" +
+        currDate.getSeconds(),
+      year: new Date().getFullYear(),
+    },
+    metric: "aa-pageload",
+  });
+});
 
+document.querySelectorAll("button").forEach((item, index) => {
+  item.addEventListener("click", (e) => {
+    if (e.target.id != "submit" && e.target.id != "addToCart") {
+      adobeDataLayer.push({
+        link: {
+          contentSection:
+            e.target.id === "submit"
+              ? "form"
+              : e.target.id === "readMore"
+              ? "description"
+              : e.target.id === "addToCart"
+              ? "productComponent"
+              : "",
+          contentSubsection: "",
+          linkText: e.target.textContent,
+          linkComponent:
+            e.target.id === "submit"
+              ? "form"
+              : e.target.id === "readMore"
+              ? "description"
+              : e.target.id === "addToCart"
+              ? "productComponent"
+              : "",
+          linkIndex: index + 1,
+        },
+        metric:
+          e.target.id === "addToCart" ? "aa-action,aa-addtocart" : "aa-action",
+        event: "aa-action",
+        user: {
+          email: userEmail,
+        },
+      });
+    }
+    if (e.target.id === "addToCart") {
+      adobeDataLayer.push({
+        link: {
+          contentSection:
+            e.target.id === "submit"
+              ? "form"
+              : e.target.id === "readMore"
+              ? "description"
+              : e.target.id === "addToCart"
+              ? "productComponent"
+              : "",
+          contentSubsection: "addToCart",
+          linkText: e.target.textContent,
+          linkComponent:
+            e.target.id === "submit"
+              ? "form"
+              : e.target.id === "readMore"
+              ? "description"
+              : e.target.id === "addToCart"
+              ? "productComponent"
+              : "",
+          linkIndex: index + 1,
+        },
+        cart: {
+          productName: "bat",
+          price: "12.25",
+          productId: "w3c1234",
+          category: "ecommerce",
+          quantity: cartCount,
+        },
+        metric:
+          e.target.id === "addToCart" ? "aa-action,aa-addtocart" : "aa-action",
+        event: "aa-action",
+        user: {
+          email: userEmail,
+        },
+      });
+    }
+  });
+});
 
-window.addEventListener('load',()=>{
+document.querySelectorAll("a").forEach((item, index) => {
+  item.addEventListener("click", (e) => {
     adobeDataLayer.push({
-        "event":"aa-pageload",
-        "page":{
-            "pageInfo":{
-                "pageName":title,
-                "pageUrl":url,
-                "referrer": document.referrer,
-                "hostName":hostName,
-                "language":"en-us",
-                "errorPage": false,
-                "pageType": title,
-                "pageId":title+" "+123
-            }
-        },
-        "timeStamp":{
-            "date": currDate.toDateString(),
-            "day": Day[currDate.getDay()],
-            "month":months[currDate.getMonth()],
-            "time": currDate.getHours() + ":" + currDate.getMinutes() + ":0" + currDate.getSeconds(),
-            "year" : new Date().getFullYear(),
-    
-        },
-        "metric": "aa-pageload"
-      
-      })   
-      
- });
-
- document.querySelectorAll("button").forEach((item,index)=>{item.addEventListener("click",(e)=>{
-     if(e.target.id!="submit" && e.target.id!="addToCart"){
-                adobeDataLayer.push({
-                    link:{
-                        "contentSection":e.target.id==="submit"?"form":e.target.id==="readMore"?"description":e.target.id==="addToCart"?"productComponent":"",
-                        "contentSubsection":"",
-                        "linkText":e.target.textContent,
-                        "linkComponent":e.target.id==="submit"?"form":e.target.id==="readMore"?"description":e.target.id==="addToCart"?"productComponent":"",
-                        "linkIndex":index+1,
-                    },
-                    "metric":e.target.id==="addToCart"? "aa-action,aa-addtocart":"aa-action",
-                    "event":"aa-action"
-                });
-            }
-        if(e.target.id==="addToCart"){
-            adobeDataLayer.push({
-                link:{
-                    "contentSection":e.target.id==="submit"?"form":e.target.id==="readMore"?"description":e.target.id==="addToCart"?"productComponent":"",
-                    "contentSubsection":"addToCart",
-                    "linkText":e.target.textContent,
-                    "linkComponent":e.target.id==="submit"?"form":e.target.id==="readMore"?"description":e.target.id==="addToCart"?"productComponent":"",
-                    "linkIndex":index+1,
-                },
-                cart:{
-                    "productName":"bat",
-                    "price":"12.25",
-                    "productId":"w3c1234",
-                    "category":"ecommerce",
-                    "quantity":cartCount
-                },
-                "metric":e.target.id==="addToCart"? "aa-action,aa-addtocart":"aa-action",
-                "event":"aa-action"
-            });
-
-        }
-        
- })
-});
-
-document.querySelectorAll("a").forEach((item,index)=>{
-
-    item.addEventListener('click',(e)=>{
-
-        adobeDataLayer.push({
-            link:{
-                "contentSection":e.target.parentNode.getAttribute("id"),
-                "contentSubsection":"",
-                "linkText":e.target.textContent?e.target.textContent:"banner",
-                "linkComponent":e.target.parentNode.getAttribute("id"),
-                "linkIndex":index+1,
-            },
-            "metric":e.target.parentNode.getAttribute("id")==="footer"?"aa-link,aa-footer,aa-social":"aa-link,aa-banner-clickout",
-            "event":"aa-link"
-        });
+      link: {
+        contentSection: e.target.parentNode.getAttribute("id"),
+        contentSubsection: "",
+        linkText: e.target.textContent ? e.target.textContent : "banner",
+        linkComponent: e.target.parentNode.getAttribute("id"),
+        linkIndex: index + 1,
+      },
+      metric:
+        e.target.parentNode.getAttribute("id") === "footer"
+          ? "aa-link,aa-footer,aa-social"
+          : "aa-link,aa-banner-clickout",
+      event: "aa-link",
+      user: {
+        email: userEmail,
+      },
     });
+  });
 });
 
-document.getElementById("personalInfoForm").addEventListener("click",(e)=>{
-    e.preventDefault();
-    if(formStart===1 && e.target.id!="submit"){
-        adobeDataLayer.push({
-            journey:{
-                "serviceFlowName":e.target.parentNode.parentNode.getAttribute("id"),
-                "formCategory":"personal info",
-                "firstFieldInteracted":e.target.id,
-            },
-            "page":{
-                "pageInfo":{
-                    "pageName":title,
-                    "pageUrl":url,
-                    "referrer": document.referrer,
-                    "hostName":hostName,
-                    "pageId":title+" "+123
-                }
-            },
-            "metric":"aa-pi-start,aa-journey-start",
-            "event":"aa-journey-start"
-        });
-        formStart++;
-    }
-});
-
-document.getElementById("submit").addEventListener("click",(e)=>{
-
-var name = document.getElementById("name");
-var email =    document.getElementById("email");
-var mobile =  document.getElementById("mobile");
-var region =  document.getElementById("address");
-    if(name ==="" || email  ===""|| mobile  ===""|| region  ===""){
-        alert("field cannot be empty");
-        return false;
-    }
+document.getElementById("personalInfoForm").addEventListener("click", (e) => {
+  e.preventDefault();
+  if (formStart === 1 && e.target.id != "submit") {
     adobeDataLayer.push({
-        journey:{
-            "serviceFlowName":e.target.parentNode.getAttribute("id"),
-            "formCategory":"personal info",
-            "journeyIndex":"1",
-            "journeyLength":"1",
-            "region":region.value,
-            "name":name.value,
-            "email":email.value,
-            "mobile":mobile.value
+      journey: {
+        serviceFlowName: e.target.parentNode.parentNode.getAttribute("id"),
+        formCategory: "personal info",
+        firstFieldInteracted: e.target.id,
+      },
+      page: {
+        pageInfo: {
+          pageName: title,
+          pageUrl: url,
+          referrer: document.referrer,
+          hostName: hostName,
+          pageId: title + " " + 123,
         },
-        "page":{
-            "pageInfo":{
-                "pageName":title,
-                "pageUrl":url,
-                "referrer": document.referrer,
-                "hostName":hostName,
-                "pageId":title+" "+123
-            }
-        },
-        "metric":"aa-pi-tracking,aa-journey-tracking",
-        "event":"aa-journey-tracking"
+      },
+      metric: "aa-pi-start,aa-journey-start",
+      event: "aa-journey-start",
+      user: {
+        email: userEmail,
+      },
     });
-    var check = Math.floor((Math.random() * 10));
-    console.log(check)
-
-    if((check) != 3){
-        adobeDataLayer.push({
-            journey:{
-                "serviceFlowName":e.target.parentNode.getAttribute("id"),
-                "formCategory":"personal info",
-                "journeyIndex":"1",
-                "journeyLength":"1",
-                "region":region.value,
-                "name":name.value,
-                "email":email.value,
-                "mobile":mobile.value,
-                "successMessage":"form submit success",
-                "successId":Math.floor(Math.random() * Date.now())
-            },
-            "page":{
-                "pageInfo":{
-                    "pageName":title,
-                    "pageUrl":url,
-                    "referrer": document.referrer,
-                    "hostName":hostName,
-                    "pageId":title+" "+123
-                }
-            },
-            "metric":"aa-pi-success,aa-journey-success",
-            "event":"aa-journey-success"
-        });
-       
-
-    }
-    else{
-        adobeDataLayer.push({
-            journey:{
-                "serviceFlowName":e.target.parentNode.getAttribute("id"),
-                "formCategory":"personal info",
-                "journeyIndex":"1",
-                "journeyLength":"1",
-                "region":region.value,
-                "name":name.value,
-                "email":email.value,
-                "mobile":mobile.value,
-                "failureMessage":"form submit failed",
-                "failureId":Math.floor(Math.random() * Date.now())
-            },
-            "page":{
-                "pageInfo":{
-                    "pageName":title,
-                    "pageUrl":url,
-                    "referrer": document.referrer,
-                    "hostName":hostName,
-                    "pageId":title+" "+123
-                }
-            },
-            "metric":"aa-pi-failure,aa-journey-failure",
-            "event":"aa-journey-failure"
-        });
-       
-
-    }
-        name.value = "";
-        region.value="";
-        mobile.value="";
-        email.value="";
-
+    formStart++;
+  }
 });
 
+document.getElementById("submit").addEventListener("click", (e) => {
+  var name = document.getElementById("name");
+  var email = document.getElementById("email");
+  var mobile = document.getElementById("mobile");
+  var region = document.getElementById("address");
+  if (name === "" || email === "" || mobile === "" || region === "") {
+    alert("field cannot be empty");
+    return false;
+  }
+  adobeDataLayer.push({
+    journey: {
+      serviceFlowName: e.target.parentNode.getAttribute("id"),
+      formCategory: "personal info",
+      journeyIndex: "1",
+      journeyLength: "1",
+      region: region.value,
+      name: name.value,
+      email: email.value,
+      mobile: mobile.value,
+    },
+    page: {
+      pageInfo: {
+        pageName: title,
+        pageUrl: url,
+        referrer: document.referrer,
+        hostName: hostName,
+        pageId: title + " " + 123,
+      },
+    },
+    metric: "aa-pi-tracking,aa-journey-tracking",
+    event: "aa-journey-tracking",
+    user: {
+      email: userEmail,
+    },
+  });
+  var check = Math.floor(Math.random() * 10);
+  console.log(check);
+
+  if (check != 3) {
+    adobeDataLayer.push({
+      journey: {
+        serviceFlowName: e.target.parentNode.getAttribute("id"),
+        formCategory: "personal info",
+        journeyIndex: "1",
+        journeyLength: "1",
+        region: region.value,
+        name: name.value,
+        email: email.value,
+        mobile: mobile.value,
+        successMessage: "form submit success",
+        successId: Math.floor(Math.random() * Date.now()),
+      },
+      page: {
+        pageInfo: {
+          pageName: title,
+          pageUrl: url,
+          referrer: document.referrer,
+          hostName: hostName,
+          pageId: title + " " + 123,
+        },
+      },
+      metric: "aa-pi-success,aa-journey-success",
+      event: "aa-journey-success",
+      user: {
+        email: userEmail,
+      },
+    });
+  } else {
+    adobeDataLayer.push({
+      journey: {
+        serviceFlowName: e.target.parentNode.getAttribute("id"),
+        formCategory: "personal info",
+        journeyIndex: "1",
+        journeyLength: "1",
+        region: region.value,
+        name: name.value,
+        email: email.value,
+        mobile: mobile.value,
+        failureMessage: "form submit failed",
+        failureId: Math.floor(Math.random() * Date.now()),
+      },
+      page: {
+        pageInfo: {
+          pageName: title,
+          pageUrl: url,
+          referrer: document.referrer,
+          hostName: hostName,
+          pageId: title + " " + 123,
+        },
+      },
+      metric: "aa-pi-failure,aa-journey-failure",
+      event: "aa-journey-failure",
+      user: {
+        email: userEmail,
+      },
+    });
+  }
+  name.value = "";
+  region.value = "";
+  mobile.value = "";
+  email.value = "";
+});
--- cartView.js
+++ cartView.js
@@ -1,81 +1,95 @@
-var selectedItems = localStorage?.getItem('selectedItems');
-var childrenArray=[];
-var products=[];
+var userEmail = localStorage.getItem("user_email") || "guest";
+var selectedItems = localStorage?.getItem("selectedItems");
+var childrenArray = [];
+var products = [];
 
-
-document.querySelectorAll(".product").forEach((item,index)=>{
+document.querySelectorAll(".product").forEach((item, index) => {
   var children = item.children;
-  for(let i=0;i<children.length;i++){
-  if(children[i].getAttribute("class") === "product-title"){
-      !selectedItems.includes(children[i].textContent)?item.classList.add("hide"):"";
-      selectedItems.includes(children[i].textContent)?childrenArray.push(item):"";
+  for (let i = 0; i < children.length; i++) {
+    if (children[i].getAttribute("class") === "product-title") {
+      !selectedItems.includes(children[i].textContent)
+        ? item.classList.add("hide")
+        : "";
+      selectedItems.includes(children[i].textContent)
+        ? childrenArray.push(item)
+        : "";
     }
   }
-
 });
 
-document.querySelectorAll(".product-button").forEach((item,index)=>{
-    item.addEventListener("click",(element)=>{
-       item.parentElement.classList.add("hide");
-       var children = item.parentNode.children;
-       for(let i = 0;i<children.length;i++){
-             children[i].getAttribute("class") === "product-title"?title=children[i].textContent: children[i].getAttribute("class") === "product-price"? price=children[i].textContent:"";
-             }
-             products= products.filter((i)=>i.productName!=title);
-             adobeDataLayer.push({
-                "event":"aa-cartRemoval",
-                "cart": {
-                    "productCategory":title==="HP Pavilion"?"Hp Laptops":"dell Laptops",
-                    "productId":title==="HP Pavilion"?"15678905432":"2132342456",
-                    "quantity":"1",
-                    "productPrice":price,
-                    "productName":title
-            },
-                "metric": "aa-cartRemoval"
-              
-              }) 
-    });
-});
-
-window.addEventListener('load',()=>{
-
-        childrenArray.forEach((item)=>{
-            var children = item.children;
-      for(let i = 0;i<children.length;i++){
-            children[i].getAttribute("class") === "product-title"?title=children[i].textContent: children[i].getAttribute("class") === "product-price"? price=children[i].textContent:"";
-            }
-            var obj={
-                    "productCategory":title==="HP Pavilion"?"Hp Laptops":"dell Laptops",
-                    "productId":title==="HP Pavilion"?"15678905432":"2132342456",
-                    "quantity":"1",
-                    "productPrice":price,
-                    "productName":title
-            };
-           products.push(obj);
-            
-    });
-
-    if(products.length===0){
-        return;
+document.querySelectorAll(".product-button").forEach((item, index) => {
+  item.addEventListener("click", (element) => {
+    item.parentElement.classList.add("hide");
+    var children = item.parentNode.children;
+    for (let i = 0; i < children.length; i++) {
+      children[i].getAttribute("class") === "product-title"
+        ? (title = children[i].textContent)
+        : children[i].getAttribute("class") === "product-price"
+        ? (price = children[i].textContent)
+        : "";
     }
-
+    products = products.filter((i) => i.productName != title);
     adobeDataLayer.push({
-        "event":"aa-cartView",
-        "products": products,
-        "metric": "aa-cartView"
-      
-      })   
-
-
+      event: "aa-cartRemoval",
+      user: {
+        email: userEmail,
+      },
+      cart: {
+        productCategory:
+          title === "HP Pavilion" ? "Hp Laptops" : "dell Laptops",
+        productId: title === "HP Pavilion" ? "15678905432" : "2132342456",
+        quantity: "1",
+        productPrice: price,
+        productName: title,
+      },
+      metric: "aa-cartRemoval",
+    });
+  });
 });
 
-document.getElementById("purchase").addEventListener('click',(e)=>{
-    adobeDataLayer.push({
-        "event":"aa-purchase",
-        "products": products,
-        "metric": "aa-purchase"    
-      }); 
-      alert("purchase complete");
-      e.target.classList.add("hide");
+window.addEventListener("load", () => {
+  childrenArray.forEach((item) => {
+    var children = item.children;
+    for (let i = 0; i < children.length; i++) {
+      children[i].getAttribute("class") === "product-title"
+        ? (title = children[i].textContent)
+        : children[i].getAttribute("class") === "product-price"
+        ? (price = children[i].textContent)
+        : "";
+    }
+    var obj = {
+      productCategory: title === "HP Pavilion" ? "Hp Laptops" : "dell Laptops",
+      productId: title === "HP Pavilion" ? "15678905432" : "2132342456",
+      quantity: "1",
+      productPrice: price,
+      productName: title,
+    };
+    products.push(obj);
+  });
+
+  if (products.length === 0) {
+    return;
+  }
+
+  adobeDataLayer.push({
+    event: "aa-cartView",
+    products: products,
+    metric: "aa-cartView",
+    user: {
+      email: userEmail,
+    },
+  });
 });
 
+document.getElementById("purchase").addEventListener("click", (e) => {
+  adobeDataLayer.push({
+    event: "aa-purchase",
+    user: {
+      email: userEmail,
+    },
+    products: products,
+    metric: "aa-purchase",
+  });
+  alert("purchase complete");
+  e.target.classList.add("hide");
+});
